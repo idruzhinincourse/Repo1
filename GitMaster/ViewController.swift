3b0e86cf9d17f55b0b38b57cccb6c38e0bfa8b8e
Ivan Druzhinin <dia.doca@gmail.com>
2021-11-27 22:39:35 +0300

    doSmth

--- GitMaster/ViewController.swift
+++ GitMaster/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
 
         // New Changes Here
+        doSmth()
+
+    }
+
+    private func doSmth() {
 
     }
 
